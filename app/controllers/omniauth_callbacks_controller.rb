5c25e59ae57e32acf50090f18c2d54a7723df2bb
silviodeigiudici <silviodeigiudici@gmail.com>
2018-05-28 00:56:57 +0200

    reset password with email added

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     auth = request.env["omniauth.auth"]
     user = User.where(provider: auth.provider, uid: auth.uid)
-            .first_or_initialize(email: auth.info.email, password: Devise.friendly_token[0,20], name: auth.info.name)
+            .first_or_create(email: auth.info.email, password: Devise.friendly_token[0,20], username: auth.info.name)
 
     sign_in(:user, user)
 
@@ -14,7 +14,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     auth = request.env["omniauth.auth"]
     user = User.where(provider: auth.provider, uid: auth.uid)
-            .first_or_initialize(email: auth.info.nickname, password: Devise.friendly_token[0,20], name: auth.info.nickname)
+            .first_or_create(email: auth.info.nickname + "@gmail.com", password: Devise.friendly_token[0,20], username: auth.info.nickname)
 
     sign_in(:user, user)
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,13 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  ActionMailer::Base.smtp_settings = { :address => "smtp.gmail.com", :port => 587, :authentication => :plain, :domain => 'gmail.com', :user_name => 'mydndmailer@gmail.com', :password => ENV["EMAIL_PASSWORD"] }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -31,7 +38,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
