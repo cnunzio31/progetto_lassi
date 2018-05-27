e74603eb4176d6d3cac80c0397b367fcb3646a1f
silviodeigiudici <silviodeigiudici@gmail.com>
2018-05-27 22:12:13 +0200

    Controllers added

---
+++ app/controllers/partecipations_controller.rb
@@ -0,0 +1,2 @@
+class PartecipationsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resource :home
+  resource :homes
+  resource :sessions
+  resource :matches
+  resource :invitations
+  resource :partecipations
+  resource :requests
+
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   root :to => redirect('/home')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
