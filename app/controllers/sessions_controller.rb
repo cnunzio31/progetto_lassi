2e44f450cc134672434e0d0cfee7ac414c92ff78
silviodeigiudici <silviodeigiudici@gmail.com>
2018-05-28 19:07:42 +0200

    Query added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
   def showactive
+    @activesessions = Session.where(:status => 2)
   end
   def showclosed
+    @closedsessions = Session.where(:status => 3)
   end
   def showcreated
+    @createdsessions = Session.where(:status => 1)
   end
 end
