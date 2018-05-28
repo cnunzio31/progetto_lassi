fe1ce874b037c4d6824f439e3e50050b0b4c7310
silviodeigiudici <silviodeigiudici@gmail.com>
2018-05-28 18:48:41 +0200

    Sessions pages added and path fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,8 @@
 class SessionsController < ApplicationController
+  def showactive
+  end
+  def showclosed
+  end
+  def showcreated
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-  resource :home
-  resource :sessions
-  resource :matches
-  resource :invitations
-  resource :partecipations
-  resource :requests
-
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+
+  resource :home
+  resource :sessions do
+    resource :matches
+    resource :invitations
+    resource :partecipations
+    resource :requests
+  end
+  #resources :movies do
+  #  resources :reviews, only: [:new, :create, :destroy, :like]
+  #end
+
+  get 'activesessions', to: 'sessions#showactive', as: 'active_sessions'
+  get 'createdsessions', to: 'sessions#showcreated', as: 'created_sessions'
+  get 'closedsessions', to: 'sessions#showclosed', as: 'closed_sessions'
+
   root :to => redirect('/home')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
