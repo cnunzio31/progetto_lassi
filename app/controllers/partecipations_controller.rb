0150c6b1077cea18aed9f0d81a203ee89f599235
silviodeigiudici <32673321+silviodeigiudici@users.noreply.github.com>
2018-06-03 18:17:58 +0200

    Fix controller partecipation

--- app/controllers/partecipations_controller.rb
+++ app/controllers/partecipations_controller.rb
@@ -5,7 +5,7 @@ class PartecipationsController < ApplicationController
     #post per creare una nuova partecipazione, associata all'index del request e al tasto join del player
     r_id = params[:session_id] #to change
     request = Request.find(r_id)
-    if current_user.has_role?(:master) and Session.find(r_id).master_id != current_user.id
+    if current_user.has_role?(:master) and Session.find(request.session_id).master_id != current_user.id
       redirect_to home_path
     elsif current_user.has_role?(:player) and request.player_id == current_user.id
       redirect_to home_path
