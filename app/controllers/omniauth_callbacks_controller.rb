d417f13e036799006fba66ed7c1763083915d866
silviodeigiudici <silviodeigiudici@gmail.com>
2018-06-11 21:49:48 +0200

    Fix pages

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     auth = request.env["omniauth.auth"]
     user = User.where(provider: auth.provider, uid: auth.uid)
-            .first_or_create(email: auth.info.nickname + "@gmail.com", password: Devise.friendly_token[0,20], username: auth.info.nickname)
+            .first_or_create(email: auth.info.nickname + "@github.com", password: Devise.friendly_token[0,20], username: auth.info.nickname)
 
     sign_in(:user, user)
 
